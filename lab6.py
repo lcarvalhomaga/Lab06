3e3f49bca4172f53b658b74bef9976a622931c56
lcarvalhomaga <lcarvalhomagalha@ufl.edu>
2024-10-22 16:01:50 -0500

    Added main and encode

---
+++ lab6.py
@@ -0,0 +1,26 @@
+# Lucca Carvalho Magalhaes
+def encoder(passwd):
+    encoded = ""
+    for i in passwd:
+        encoded += str((int(i) + 3) % 10) 
+    return encoded
+
+if __name__ == "__main__":
+    while True:
+       
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        print()
+
+        opt = int(input("Please enter an option: "))
+        passwd = "1"
+        if opt == 1:
+            passwd = input("lease enter your password to encode: ")
+            passwd = encoder(passwd)
+        elif opt == 2:
+            decoder(passwd)
+        elif opt == 3:
+            break
